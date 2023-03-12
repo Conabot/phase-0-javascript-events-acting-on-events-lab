e93bda4095d1d35009fcbc429355ff06f7cba365
Minh Tran <tdaimike@gmail.com>
2023-03-12 14:01:00 -0700

    complete lab

--- index.js
+++ index.js
@@ -9,12 +9,7 @@ function moveDodgerLeft() {
       dodger.style.left = `${left - 1}px`;
     }
   }
-  
-  document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowLeft") {
-      moveDodgerLeft();
-    }
-  });
+
 
   function moveDodgerRight() {
     const leftNumbers = dodger.style.left.replace("px", "");
@@ -27,6 +22,10 @@ function moveDodgerLeft() {
   
   document.addEventListener("keydown", function (e) {
     if (e.key === "ArrowLeft") {
-      moveDodgerRight();
+      moveDodgerLeft();
+    }
+    if (e.key === "ArowRight") {
+        moveDodgerRight();
     }
   });
+ 
